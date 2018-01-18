a7889f2020f29322627b73b93c7edf691196fe65
ewanaszydlowska <ewanasz@gmail.com>
2018-01-18 12:41:42 +0100

    add review controller and repo

---
+++ src/main/java/pl/kupujswiadomie/controller/ReviewController.java
@@ -0,0 +1,22 @@
+package pl.kupujswiadomie.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.kupujswiadomie.entity.Category;
+import pl.kupujswiadomie.entity.Product;
+import pl.kupujswiadomie.repository.CategoryRepository;
+import pl.kupujswiadomie.repository.ProductRepository;
+
+@Controller
+@RequestMapping("review")
+public class ReviewController {
+	
+		
+}
